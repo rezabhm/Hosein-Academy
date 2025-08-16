0f0244184ad0f5aef7574ee22362f7929f685c2b
Mobin Anooshi <mobinanooshi2003@gmail.com>
2025-08-16 16:27:17 +0330

    create Textbook model

--- apps/textbook/admin.py
+++ apps/textbook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TextBook
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(TextBook)
+class TextBookAdmin(admin.ModelAdmin):
+    list_display = ("title", "pdf_file")  # Fields displayed in list view
+    search_fields = ("title", "body")     # Searchable fields in admin
+    help_text=_("Teachers associated with this course.")
+    
--- apps/textbook/models.py
+++ apps/textbook/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apps.core.models import BaseModel
 
-# Create your models here.
+class TextBook(BaseModel):
+    """
+    Stores information about textbooks.
+    Each textbook has a title, body (description/content),
+    and an uploaded PDF file.
+    """
+    title = models.CharField(max_length=255)  # Title of the textbook
+    body = models.TextField()                 # Description or content
+    pdf_file = models.FileField(upload_to="textbooks/", verbose_name="PDF File")  
+    # FileField with upload path set to 'media/textbooks/'
+
+    def __str__(self):
+        return f'Title: {self.title}'
